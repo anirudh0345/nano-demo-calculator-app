9a17d95a39c0f079203acd6342fb9b8272f17afa
anirudh0345 <92367503+anirudh0345@users.noreply.github.com>
2023-08-28 00:09:40 +0530

    Add files via upload

---
+++ serve_calc.py
@@ -0,0 +1,29 @@
+
+from flask import Flask
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
+
+app = Flask(__name__)
+
+def greeting():
+    return 'Hello world!'
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
+
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
